e1dd376ab6e5c2f5a44d53ce29ee1bae85750f93
Shawn Inder <shawninder@gmail.com>
2021-04-12 12:24:33 -0400

    commit built files

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'a6cf6830db0e583fb4e67d5b0c37ac79');
