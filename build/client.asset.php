3c5c9073f4b9ef90637ee7cf78744f96e5ca42be
Shawn Inder <shawninder@gmail.com>
2021-04-25 00:42:45 -0400

    build

--- build/client.asset.php
+++ build/client.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'cd701e43b7b4a55c600ad62c3d5cb1f4');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'deffc5bfa2b141d855bd53c428cb6643');
