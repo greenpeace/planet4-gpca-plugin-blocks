e0a4b2e153a176611aac57877ab5c0ed35f988d6
Shawn Inder <shawninder@gmail.com>
2021-04-27 13:42:09 -0400

    Inline SVG -> CSS data URI SVG

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '733773ab0f0694e39a637a57d0059021');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3ee27350933e230c36b68a45ee696868');
